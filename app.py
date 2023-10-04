89a96b7c46babca30e87c1b8a8710650d8dcece0
Eswar Aditya <142793639+EswarAditya5@users.noreply.github.com>
2023-10-04 12:56:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,11 +5,8 @@ import pickle
 import streamlit as st
 
 st.session_state['answer'] = ''!
-
 st.write(st.session_state)
-
 realans = ['', 'abc', 'edf']
-
 if  st.session_state['answer'] in realans:
     answerStat = "correct"
 elif st.session_state['answer'] not in realans:
